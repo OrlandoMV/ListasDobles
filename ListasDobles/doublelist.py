06d83acc48f855d507f2a9b1647ae2b1ba401c6c
Orlando Martínez <i24050386@monclova.tecnm.mx>
2025-10-22 18:21:13 -0600

    Update doublelist.py

--- ListasDobles/doublelist.py
+++ ListasDobles/doublelist.py
@@ -3,13 +3,18 @@ from node import Node
 class DoubleList:
     def __init__(self):
         self._head = None
-        self._lastnode = None
 
-    
-    def add(self, n):
+    @property
+    def head(self):
+        return self._head
+
+    @head.setter
+    def head(self, value):
+        self._head = value
+
+    def add(self, n: Node):
         if self._head is None:
             self._head = n
-            self._lastnode = n  
             return
 
         if n.data < self._head.data:
@@ -23,43 +28,43 @@ class DoubleList:
             if n.data < h.next.data:
                 n.next = h.next
                 n.back = h
+                h.next.back = n
                 h.next = n
-                n.next.back = n
                 return
             h = h.next
 
         h.next = n
         n.back = h
-        self._lastnode = n  
 
+    def remove(self, data: int):
+        if self._head is None:
+            return
 
-    def remove(self, data):
-        current = self._head
+        if self._head.data == data:
+            if self._head.next is not None:
+                self._head.next.back = None
+            self._head = self._head.next
+            return
 
-        while current is not None:
-            if current.data == data:
-                if current.back is not None:
-                    current.back.next = current.next
-                else:
-                    self._head = current.next
-                if current.next is not None:
-                    current.next.back = current.back
-                else:
-                    self._lastnode = current.back
-                return 
-            current = current.next
+        h = self._head
+        while h.next is not None:
+            if h.next.data == data:
+                temp = h.next
+                h.next = temp.next
+                if temp.next is not None:
+                    temp.next.back = h
+                return
+            h = h.next
 
-    def exists(self, data):
+    def exists(self, data: int) -> bool:
         h = self._head
         while h is not None:
             if h.data == data:
                 return True
-            if data < h.data:
-                return False
             h = h.next
         return False
 
-    def count(self):
+    def count(self) -> int:
         count = 0
         h = self._head
         while h is not None:
@@ -67,9 +72,10 @@ class DoubleList:
             h = h.next
         return count
 
-    def show(self):
+    def show(self) -> str:
         if self._head is None:
             return "Empty List"
+
         i = 0
         h = self._head
         datas = ""
@@ -79,21 +85,29 @@ class DoubleList:
             h = h.next
         return datas
 
-    def reverse_show(self):
+    def reverse_show(self) -> str:
         if self._head is None:
             return "Empty List"
-        l = self._lastnode
+
+        h = self._head
+        last = None
         i = 0
-        datas = ""
-        while l is not None:
-            datas += f"Node [{i}] and {l}\n"
-            l = l.back
+        while h is not None:
+            last = h
+            h = h.next
             i += 1
+
+        datas = ""
+        i -= 1
+        h = last
+        while h is not None:
+            datas += f"Node [{i}] {h}\n"
+            h = h.back
+            i -= 1
         return datas
 
     def clear(self):
         self._head = None
-        self._lastnode = None
 
     def __str__(self):
         result = ""
